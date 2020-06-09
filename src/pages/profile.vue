f304c5412d756256d4e788b65f05cf36f7adcca0
Ruchie V. Rubia <rrubia@fullscale.io>
2020-06-10 02:10:26 +0800

    Implement q-card and other components to show characters

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,7 +1,25 @@
 <template>
   <q-page padding>
     <!-- content -->
-    <h3>{{character.name}}</h3>
+    <div class="column justify-center items-center">
+      <h3>{{character.name}}</h3>
+      <q-card class="my-card">
+        <img :src="character.img" alt="">
+        <q-card-section>
+          <div class="text-h6">{{character.name}}</div>
+          <div class="text-subtitle2">{{character.nickname}}</div>
+        </q-card-section>
+        <q-card-section>
+          <q-chip v-for = "occupation in character.occupation"
+            color="teal"
+            :key="occupation"
+            text-color="white"
+            icon="bookmark">
+            {{occupation}}
+          </q-chip>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
@@ -23,3 +41,8 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  .my-card {
+    width: 20%;
+  }
+</style>
