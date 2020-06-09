727af589d6a79e36d0496e676d1bbeb21c168fde
Ruchie V. Rubia <rrubia@fullscale.io>
2020-06-10 01:47:24 +0800

    Fix url for random character api

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -9,7 +9,11 @@
 import axios from '../boot/axios'
 export default {
   async created() {
-    const character = await this.$axios.get(`https://www.breakingbadapi.com/api/characters/${this.$route.params.id}`);
+    let url = `https://www.breakingbadapi.com/api/characters/${this.$route.params.id}`;
+    if (this.$route.params.id === 'random') {
+      url = `https://www.breakingbadapi.com/api/character/${this.$route.params.id}`;
+    }
+    const character = await this.$axios.get(url);
     this.character = character.data[0];
   },
   data() {
